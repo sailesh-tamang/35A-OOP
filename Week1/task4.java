b89e78f618ca409c5aa98ed7dac1de2b115fe153
sailesh-tamang <saileshtamang909@gmail.com>
2024-04-11 21:34:02 +0545

    week 1 task 4 done

---
+++ Week1/task4.java
@@ -0,0 +1,19 @@
+package Week1;
+
+public class task4 {
+    public static void main(String args[]) {
+        /*4. WAP that prints the following pattern:
+
+*
+
+**
+
+***
+
+**** */
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }    
+}
