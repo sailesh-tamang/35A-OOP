1e106fb3aeb7e825fe7133a94daea21fb12e7647
sailesh-tamang <saileshtamang909@gmail.com>
2024-04-11 10:49:32 +0545

    task 3

---
+++ Week1/task3.java
@@ -0,0 +1,13 @@
+package Week1;
+
+public class task3 {
+    public static void main(String[] args) {
+        System.out.println("A \"quote\" string is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rules of \"escape sequences.\" ");
+        System.out.println("Also, \"\" represents an empty");
+        System.out.println("String. Don't forget: use \\\" instead of \"!");
+        System.out.println("'' is not the same as \"");
+    }
+    
+}
