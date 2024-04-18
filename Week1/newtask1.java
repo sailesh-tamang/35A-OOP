d48e1cab1050c5e91a7196a79a720e3fa62459a3
sailesh-tamang <saileshtamang909@gmail.com>
2024-04-18 10:30:42 +0545

    new task 1 done of operators

---
+++ Week1/newtask1.java
@@ -0,0 +1,14 @@
+package Week1;
+
+public class newtask1 {
+    public static void main(String[] args) {
+        int a = 20;
+        if (a>=18 ) {
+            System.out.println("the value of ais greater than or equal to 18");
+
+        }else {
+            System.out.println("the value of a is less than 18");
+        }
+    }
+    
+}
