be50493d59422eb29b8bf50b3c67ebc83e2f272b
sailesh-tamang <saileshtamang909@gmail.com>
2024-04-18 10:47:09 +0545

    new task 4

---
+++ Week1/newtask4.java
@@ -0,0 +1,12 @@
+package Week1;
+
+public class newtask4 {
+    public static void main(String[] args) {
+        int a= 10, b= 20;
+        String output = a>=b? "a is greater" :"b is greater";
+        System.out.println(output);
+
+
+    }
+    
+}
