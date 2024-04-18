907daaf5c88ffa8a51fe4291c639fb5af41e5424
sailesh-tamang <saileshtamang909@gmail.com>
2024-04-18 10:46:31 +0545

    new task 2

---
+++ Week1/newtask2.java
@@ -0,0 +1,13 @@
+package Week1;
+
+public class newtask2 {
+    public static void main(String[] args) {
+        int a= 10;
+        int b= 20;
+        int c= 30;
+        int si=a*b+c/100;
+        System.out.println(si);
+    
+    }
+}
+
