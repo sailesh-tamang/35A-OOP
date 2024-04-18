8d65ff24d5b818fb8852b49d66bb8b55b0511563
sailesh-tamang <saileshtamang909@gmail.com>
2024-04-18 10:46:08 +0545

    newtask 1

--- Week1/newtask1.java
+++ Week1/newtask1.java
@@ -4,7 +4,7 @@ public class newtask1 {
     public static void main(String[] args) {
         int a = 20;
         if (a>=18 ) {
-            System.out.println("the value of ais greater than or equal to 18");
+            System.out.println("the value of is greater than or equal to 18");
 
         }else {
             System.out.println("the value of a is less than 18");
