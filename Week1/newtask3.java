25b57c5a305fbe6d0f9b73a74a17043c809f5c33
sailesh-tamang <saileshtamang909@gmail.com>
2024-04-18 10:46:52 +0545

    new task 3

---
+++ Week1/newtask3.java
@@ -0,0 +1,14 @@
+package Week1;
+
+public class newtask3 {
+    public static void main(String[] args) {
+        int l=10;
+        int b =20;
+        int area=l*b;
+        int perimeter=2*(l+b);
+        System.out.println(area);
+        System.out.println(perimeter);
+
+    }
+    
+}
